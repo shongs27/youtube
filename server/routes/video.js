1412c8b8331910da0010a9cc79154c3a7fe5d5b1
shongs27 <shongs27@naver.com>
2021-03-20 19:00:16 +0900

    fixed videoUploadPage

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,41 +4,44 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 
-let storage = multer.diskStorage({
+var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
+    console.log(file);
     cb(null, `${Date.now()}_${file.originalname}`);
   },
-  // fileFilter: (req, file, cb) => {
-  //   const ext = path.extname(file.originalname);
 
-  //   // if (ext !== ".mp4") {
-  //   //   return cb(res.status(400).end("mp4 파일만 등록할 수 있습니다"), false);
-  //   // }
-  //   cb(null, true);
-  // },
+  fileFilter: (req, file, cb) => {
+    //왜 png, jpg 파일은 안걸릴까?
+    const ext = path.extname(req.file.filename);
+    console.log("안녕");
+    console.log(ext);
+    if (ext !== ".mp4") {
+      return cb(res.status(400).end("only jpg, png, mp4 is allowed"), false);
+    } else {
+      cb(null, true);
+    }
+  },
 });
-var upload = multer({ storage: storage }).single("file"); //파일은 하나씩만
+var upload = multer({ storage: storage }).single("file");
 
 //=================================
 //             Video
 //=================================
 
 router.post("/uploadfiles", (req, res) => {
-  // 비디오를 서버에 저장한다
-  //npm i multer
-
-  upload((req, res, err) => {
+  upload(req, res, (err) => {
     if (err) {
       return res.json({ success: false, err });
     }
-
+    const ext = path.extname(res.req.file.filename);
     return res.json({
       success: true,
-      // filePath: res.req.file.path,
-      // fileName: res.req.file.filename,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+      fileExt: ext,
     });
   });
 });
